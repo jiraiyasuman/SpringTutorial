6ab7bf2ffb8b663e4a39d9779649a520fb66fe10
jiraiyasuman <suman.talukdar53@gmail.com>
2025-02-08 21:47:49 +0530

    updated

--- 05-spring-boot-rest-security-jdbc-bcrypt/src/main/java/com/springootrestcrud/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springootrestcrud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springootrestcrud.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
-
-}
